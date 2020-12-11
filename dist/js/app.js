9a1839742a6d0f141a520cd1b0f6edabb224b277
Macu <mariacladiaperezes@gmail.com>
2020-12-11 00:46:14 -0300

    se agrego el accordion y la funcionalidad

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,11 @@
 const header = document.querySelector('.header');
+const  accordion = document.querySelector('#accordion');
 
 window.addEventListener('scroll', changeColorNav);
+accordion.addEventListener('click', showCloseContent);
 
 
 function changeColorNav () {
-  console.log(window.scrollY);
   if(window.scrollY >= 53){
     header.style.backgroundColor = '#232526';
     header.style.borderBottom = 'none';
@@ -13,3 +14,22 @@ function changeColorNav () {
     header.style.borderBottom = '1px solid #E0752F';
   }
 }
+
+
+//accordion 
+function showCloseContent (e) {
+  if(e.target.classList.contains('card-header__button') || e.target.classList.contains('button-img'))  {
+    const arrow =  (e.target.nodeName === 'IMG' ? e.target.parentElement : e.target)
+    const content = arrow.parentElement.parentElement.children[1];
+  
+    if(content.style.maxHeight) {
+      content.style.maxHeight = null;
+      arrow.style.transform = 'rotate(0deg)'
+    }else{
+      content.style.maxHeight = content.scrollHeight + "px";
+      arrow.style.transform = 'rotate(180deg)'
+    }
+
+  }
+}
+
